248bfb8b722ae295713a701ef5db2d674af1fcd6
aljimsondev <aljimson.sf1@gmail.com>
2024-04-24 11:00:01 +0800

    chore: added search input

--- src/components/client/banner/MainBanner.tsx
+++ src/components/client/banner/MainBanner.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import { Typography } from "src/components/ui/typography";
+import { Input } from "@components/ui/input";
+import { Typography } from "@components/ui/typography";
+import { IconSearch } from "@tabler/icons-react";
 
 function MainBanner() {
   return (
@@ -11,14 +13,29 @@ function MainBanner() {
       </div>
       <div className="absolute top-0 w-full h-full left-0 z-20">
         <div className="h-full w-full relative flex items-center justify-center text-center flex-col">
-          <Typography variant="h3" className="font-bold">
-            CREATE, DISCOVER.
-          </Typography>
-          <Typography variant="h1">BECOME</Typography>
-        </div>
-        <div>
-          <div></div>
-          <div></div>
+          <div>
+            <Typography variant="h3" className="font-bold">
+              CREATE, DISCOVER.
+            </Typography>
+            <Typography variant="h1">BECOME</Typography>
+          </div>
+          <div className="my-4">
+            <div>
+              <div className="w-[500px] bg-white h-[60px] flex items-center rounded-2xl overflow-hidden ring-1 ring-ring">
+                <span className="h-full aspect-square flex items-center justify-center text-primary-foreground">
+                  <IconSearch stroke={2} />
+                </span>
+                <Input
+                  className="h-full flex-1 text-primary-foreground border-none ring-0 outline-none text-lg font-semibold focus-visible:ring-0 px-0 pr-2"
+                  placeholder="Find your creators..."
+                />
+                <button className="bg-[#500000] h-full px-6 font-semibold">
+                  Search
+                </button>
+              </div>
+            </div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
