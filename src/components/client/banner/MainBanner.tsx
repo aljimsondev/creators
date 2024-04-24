8ff6c64ee628f6184941cc6f27ee32f85b097dd4
aljimsondev <aljimson.sf1@gmail.com>
2024-04-24 12:46:26 +0800

    style: adjust banner style

--- src/components/client/banner/MainBanner.tsx
+++ src/components/client/banner/MainBanner.tsx
@@ -19,19 +19,24 @@ function MainBanner() {
   const [searchText, setSearchText] = React.useState("");
 
   return (
-    <div className="h-[70vh] w-full relative -top-navbar">
+    <div className="h-[65vh] w-full relative -top-navbar">
       <div className="w-full h-full relative pointer-events-none">
-        <Image src="/assets/banner/main-banner.png" fill alt="banner" />
+        <Image
+          src="/assets/banner/main-banner.png"
+          objectFit="cover"
+          fill
+          alt="banner"
+        />
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black via-transparent to-transparent"></div>
       </div>
       <div className="absolute top-0 w-full h-full left-0 z-20">
         <Container className="h-full w-full">
           <div className="h-full w-full relative flex items-center justify-center text-center flex-col">
             <div>
-              <Typography className="font-bold text-lg md:text-5xl">
+              <Typography className="font-bold text-lg md:text-3xl lg:text-5xl">
                 CREATE, DISCOVER.
               </Typography>
-              <Typography className="font-bold text-xl md:text-9xl">
+              <Typography className="font-bold text-4xl md:text-7xl lg:text-9xl">
                 BECOME!
               </Typography>
             </div>
