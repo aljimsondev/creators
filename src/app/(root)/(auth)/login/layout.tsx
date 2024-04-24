d15b0c80952eefbd02d229d5f431cc259736d91a
aljimsondev <aljimson.sf1@gmail.com>
2024-04-24 17:46:42 +0800

    refactor: added auth page navigation in navbar

--- src/app/(root)/(auth)/login/layout.tsx
+++ src/app/(root)/(auth)/login/layout.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-function LoginPageLayout() {
-  return <div>LoginPageLayout</div>;
+function LoginPageLayout({ children }: { children: ReactNode }) {
+  return <main>{children}</main>;
 }
 
 export default LoginPageLayout;
