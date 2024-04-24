bd2e52e3567722c69163ecd96456ace61818cc69
aljimsondev <aljimson.sf1@gmail.com>
2024-04-24 12:31:13 +0800

    refactor: added search bar component

--- src/components/client/banner/MainBanner.tsx
+++ src/components/client/banner/MainBanner.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { Input } from "@components/ui/input";
 import { Typography } from "@components/ui/typography";
 import { IconSearch } from "@tabler/icons-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "src/components/ui/card";
+import { Separator } from "src/components/ui/separator";
+import Searchbar from "src/components/elements/searchbar";
 
 function MainBanner() {
+  const [searchText, setSearchText] = React.useState("");
+
   return (
     <div className="h-[70vh] bg-slate-500 w-full relative -top-navbar">
       <div className="w-full h-full relative pointer-events-none">
@@ -20,21 +32,11 @@ function MainBanner() {
             <Typography variant="h1">BECOME</Typography>
           </div>
           <div className="my-4">
-            <div>
-              <div className="w-[500px] bg-white h-[60px] flex items-center rounded-2xl overflow-hidden ring-1 ring-ring">
-                <span className="h-full aspect-square flex items-center justify-center text-primary-foreground">
-                  <IconSearch stroke={2} />
-                </span>
-                <Input
-                  className="h-full flex-1 text-primary-foreground border-none ring-0 outline-none text-lg font-semibold focus-visible:ring-0 px-0 pr-2"
-                  placeholder="Find your creators..."
-                />
-                <button className="bg-accent-secondary h-full px-6 font-semibold">
-                  Search
-                </button>
-              </div>
-            </div>
-            <div></div>
+            <Searchbar
+              value={searchText}
+              onChange={(e) => setSearchText(e?.target?.value)}
+              onClearInput={() => setSearchText("")}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/elements/searchbar/index.tsx
@@ -0,0 +1,116 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { Input } from "@components/ui/input";
+import { Typography } from "@components/ui/typography";
+import { IconSearch, IconX } from "@tabler/icons-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "src/components/ui/card";
+import { Separator } from "src/components/ui/separator";
+import { useDebounce } from "use-debounce";
+import { cn } from "src/lib/utils";
+import SearchItem from "./SearchItem";
+
+function Searchbar({
+  onClearInput = () => {},
+  ...props
+}: React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  onClearInput?: () => void;
+}) {
+  const [open, setOpen] = React.useState(false);
+  const [debouncedSearchText] = useDebounce(props?.value as string, 1000);
+  const [results, setResults] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const parentRef = React.useRef<HTMLDivElement>(null);
+
+  //todo handle open result logic here
+  React.useEffect(() => {
+    if (debouncedSearchText) {
+      return setOpen(true);
+    }
+    setOpen(false);
+
+    return () => {
+      setOpen(false);
+    };
+  }, [debouncedSearchText]);
+
+  //manages click outside listener
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        parentRef.current &&
+        !parentRef.current.contains(event.target as any)
+      ) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  console.log(props);
+  return (
+    <div
+      data-state={open ? "open" : "closed"}
+      ref={parentRef}
+      className={cn("group relative flex w-full flex-col backdrop-blur-sm")}
+    >
+      <div className="w-[500px] bg-white h-[60px] flex items-center rounded-2xl overflow-hidden ring-1 ring-ring focus-within:ring-accent-primary outline-2 outline-accent-primary">
+        <span className="h-full aspect-square flex items-center justify-center text-primary-foreground">
+          <IconSearch stroke={2} />
+        </span>
+        <Input
+          ref={inputRef}
+          className="h-full flex-1 text-primary-foreground border-none ring-0 outline-none text-lg font-semibold focus-visible:ring-0 px-0"
+          placeholder="Find your creators..."
+          {...props}
+        />
+        {props.value && (
+          <button
+            className="h-full aspect-square flex items-center justify-center text-primary-foreground"
+            onClick={onClearInput}
+          >
+            <IconX stroke={2} />
+          </button>
+        )}
+        <button className="bg-accent-secondary duration-300 hover:bg-accent-secondary/90 h-full px-6 font-semibold">
+          Search
+        </button>
+      </div>
+      <div className="absolute left-0 top-full w-full opacity-0 transition-opacity group-data-[state=closed]:pointer-events-none group-data-[state=open]:pointer-events-auto group-data-[state=open]:visible group-data-[state=clsoed]:hidden group-data-[state=open]:opacity-100">
+        <div className="w-full translate-y-0 transform overflow-hidden rounded-md shadow-md duration-300 ease-in-out group-data-[state=open]:translate-y-2">
+          <Card className="max-h-[500px] overflow-auto">
+            <div className="flex items-center justify-between p-4 sticky top-0 bg-card z-10">
+              <Typography className="font-semibold">Creators</Typography>
+              <Typography variant="subtitle1">Results: 10</Typography>
+            </div>
+            <Separator />
+            <CardContent className="text-start p-4">
+              <div>
+                {new Array(10).fill(0).map((_, index) => {
+                  return <SearchItem key={index} />;
+                })}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Searchbar;
