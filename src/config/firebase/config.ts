3a3e1383fb2ffb524fba25060565c8d68fbef069
aljimsondev <aljimson.sf1@gmail.com>
2024-04-25 08:56:14 +0800

    chore: added firebase testing database configuration

---
+++ src/config/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrfzVtrkOBV2EYClvYvuIdQRqMubnqyU4",
+  authDomain: "next-auth-implementation-535c2.firebaseapp.com",
+  projectId: "next-auth-implementation-535c2",
+  storageBucket: "next-auth-implementation-535c2.appspot.com",
+  messagingSenderId: "1048641502095",
+  appId: "1:1048641502095:web:658f76f43297ea1f8ba9fa",
+  measurementId: "G-VLP4Q1E5C7",
+};
