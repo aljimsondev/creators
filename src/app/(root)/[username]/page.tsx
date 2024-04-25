b4c9c7aad33bf017c219a0f511b308b261c9090c
aljimsondev <aljimson.sf1@gmail.com>
2024-04-25 14:08:27 +0800

    chore: added banner

--- src/app/(root)/[username]/page.tsx
+++ src/app/(root)/[username]/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { ProfileBanner } from "src/components/client/banner/profile";
+import { creators } from "src/test-data/creators";
 
-function ProfilePage() {
-  return <div>ProfilePage</div>;
+async function getUserDetails(username: string) {
+  try {
+    //replace this in getting user logic in database
+    const user = creators.find((user) => user.username === username);
+
+    return user;
+  } catch (e: any) {
+    throw new Error("Get user error: " + e);
+  }
+}
+
+async function ProfilePage({ params }: { params: { username: string } }) {
+  const username = params?.username;
+
+  const user = await getUserDetails(username);
+
+  return (
+    <Fragment>
+      <ProfileBanner bannerImage={user?.banner} />
+    </Fragment>
+  );
 }
 
 export default ProfilePage;
---
+++ src/components/client/banner/profile/ProfileBanner.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import AppImage from "src/components/elements/image/AppImage";
+import { Container } from "src/components/ui/container";
+
+function ProfileBanner({ bannerImage }: { bannerImage?: string }) {
+  return (
+    <div className="banner_gradient mb-6 w-full h-[60dvh]">
+      <div
+        className="banner__gradient_content absolute left-0 top-0 after:absolute after:h-full after:w-full after:bg-gradient-to-b after:from-black/70 after:via-transparent after:to-transparent"
+        style={{
+          backgroundImage: `url(${bannerImage})`,
+        }}
+      />
+      <Container className="h-full">
+        <div className="w-full h-full relative z-[1] rounded-lg overflow-hidden">
+          <AppImage
+            src={bannerImage!}
+            alt=""
+            fill
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default ProfileBanner;
