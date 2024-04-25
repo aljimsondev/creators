6c12bb75a37ed39accc57cd5a0be7f2f27092587
aljimsondev <aljimson.sf1@gmail.com>
2024-04-25 18:14:33 +0800

    style: adjust navbar sticky positioning and tabs sticky top

--- src/app/(root)/[username]/layout.tsx
+++ src/app/(root)/[username]/layout.tsx
@@ -4,7 +4,14 @@ import { AppNavbar } from "src/components/elements/navbar";
 function Layout({ children }: { children: ReactNode }) {
   return (
     <main>
-      <AppNavbar position="relative" />
+      <AppNavbar
+        position="sticky"
+        options={{
+          shrinkOptions: {
+            shrinkClassname: "bg-primary-foreground/20 backdrop-blur-md",
+          },
+        }}
+      />
       {children}
     </main>
   );
