b190aa84dead3c933d5c59c42699233b2c08ab1c
Al Jimson A. Megriño <108307848+aljimsondev@users.noreply.github.com>
2025-07-05 06:14:40 +0800

    Update config.ts

--- src/config/firebase/config.ts
+++ src/config/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDrfzVtrkOBV2EYClvYvuIdQRqMubnqyU4",
-  authDomain: "next-auth-implementation-535c2.firebaseapp.com",
-  projectId: "next-auth-implementation-535c2",
-  storageBucket: "next-auth-implementation-535c2.appspot.com",
-  messagingSenderId: "1048641502095",
-  appId: "1:1048641502095:web:658f76f43297ea1f8ba9fa",
-  measurementId: "G-VLP4Q1E5C7",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MSG_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
